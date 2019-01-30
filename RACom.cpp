b44e06279fa95b7ae2730692c3e7ba2dc0dbcc69
EmanueleSc <scalaemanuele92@gmail.com>
2019-01-30 12:39:26 +0100

    first algorithm implementation

--- RACom.cpp
+++ RACom.cpp
@@ -2,9 +2,16 @@
 
 SoftwareSerial MySerial (RX, TX);
 
+int initFlag;
 int MY_ID;
-int _bufsize;
-char _buffer[32];
+int currSucc;
+unsigned long ticksAtStart;
+unsigned long cmdTimeout;
+String message = "";
+
+int bufsize;
+char jsonStr[250];
+StaticJsonBuffer<200> jsonBuffer;
 
 
 void RACom::init(int id) {
@@ -17,11 +24,12 @@ void RACom::init(int id) {
 
     pinMode(SET_PIN, OUTPUT); // Connected to set input
     
-    _bufsize = sizeof(_buffer)/sizeof(char);
+    initFlag = 0;
     MY_ID = id;
+    currSucc = MY_ID;
 
-    // flush the buffer
-    memset(_buffer, 0, _bufsize);
+    bufsize = sizeof(jsonStr)/sizeof(char);
+    memset(jsonStr, 0, bufsize);
 }
 
 void RACom::comunicationMode() {
@@ -39,4 +47,105 @@ void RACom::testCom() {
   if(Serial.available()) {              // If Serial monitor has data
     MySerial.write(Serial.read());      // Send that data to HC-12
   }
+}
+
+void RACom::comunicationAlgorithm() {
+INIT:if(initFlag == 0) {
+    MySerial.flush();
+    startOperation(RING_ROUND_TRIP_TIMEOUT);
+    initFlag = 1;
+    message = "";
+  }
+
+  if(isOperationTimedOut() && message == "") {
+STEP1:findMyNext();
+    broadcast(MY_ID, currSucc);
+    startOperation(RESPONSE_TIMEOUT);
+    message = "";
+
+    while(!isOperationTimedOut()) {
+      if(MySerial.available()) {
+        if((char)MySerial.read() == '@') {
+          message = MySerial.readStringUntil('$');
+          break;
+        }
+      }
+    }
+    Serial.print("<--- Message received: ");
+    Serial.println(message);
+    
+    if(message == "") goto STEP1;
+    if(getMit(message) == currSucc) {
+      initFlag = 0;
+      goto INIT;
+    }
+
+  }
+
+  if(MySerial.available()) {
+    if((char)MySerial.read() == '@') {
+      message = MySerial.readStringUntil('$');
+      
+      Serial.print("<--- Message received: ");
+      Serial.println(message);
+
+      if(getSucc(message) == MY_ID) goto STEP1;
+      initFlag = 0; // else
+    }
+    else message = "";
+  }
+}
+
+void RACom::findMyNext() {
+  currSucc++;
+
+  if(currSucc > NUM_ANTS) currSucc = 1;
+  if(currSucc == MY_ID) currSucc++;
+
+}
+
+void RACom::broadcast(int mit, int succ) {
+  memset(jsonStr, 0, bufsize);
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["mit"] = mit;
+  root["succ"] = succ;
+  root.printTo(Serial);
+  root.printTo(jsonStr);
+
+  MySerial.print('@');
+  MySerial.print(jsonStr);
+  MySerial.print('$');
+}
+
+int RACom::getMit(String json) {
+  JsonObject& root = jsonBuffer.parseObject(json);
+  return root["mit"].as<int>();
+}
+
+int RACom::getSucc(String json) {
+  JsonObject& root = jsonBuffer.parseObject(json);
+  return root["succ"].as<int>();
+}
+
+void RACom::startOperation(unsigned long timeout) {
+    ticksAtStart = millis();
+    cmdTimeout = timeout;
+}
+
+bool RACom::isOperationTimedOut() const {
+    return operationDuration() >= cmdTimeout;
+}
+
+
+unsigned long RACom::operationDuration() const {
+    unsigned long current_ticks = millis();
+    unsigned long elapsed_ticks;
+    
+    if (current_ticks >= ticksAtStart)
+        elapsed_ticks = current_ticks - ticksAtStart;
+    else
+        elapsed_ticks = (ULONG_MAX - ticksAtStart) + current_ticks;
+
+    return elapsed_ticks;
 }
--- RACom.h
+++ RACom.h
@@ -4,13 +4,61 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include <limits.h>
+#include "ArduinoJson.h"
+
+/**
+*
+Algoritmo pseudocodice:
+
+    algorithm communication is 
+
+        init <- 0
+        message <- empty
+        mit <- my_id
+
+        while true do 
+
+INIT:            if init = 0                        
+                        start timeout_G <- 10sec
+                        init <- 1
+                
+
+                if (timeout_G expired) and (message is empty)
+STEP1:                  succ <- findMyNext
+                        broadcast { mit, succ }
+                        start timeout_R <- 200ms 
+                        message <- empty
+
+                        while not timeout_R is expired do 
+                                When receive @ read message until $ then break
+                        
+                        if message is empty
+                            go_to STEP1
+                        
+                        if message.mit = succ
+                            init <- 0
+                            go_to INIT
+                
+                if receive message
+                        When receive @ read message until $ then break
+                        
+                        if message.succ = my_id
+                            go_to STEP1
+                        
+                        init <- 0
+                        go_to INIT
+*/              
+
 
 enum
 {
+    NUM_ANTS = 2, // Number of ants in the antNet
     BAUND_RATE = 9600,
     RX = 11,
     TX = 10,
-    SET_PIN = 6
+    SET_PIN = 6,
+    RING_ROUND_TRIP_TIMEOUT = 30000, // 30 sec for test
+    RESPONSE_TIMEOUT = 500 // 500 millisec for test
 };
 
 class RACom {
@@ -19,9 +67,17 @@ public:
     void comunicationMode();
     void commandMode();
     void testCom();
+    void comunicationAlgorithm();
 
-//private:
-    
+private:
+    void findMyNext();
+    void broadcast();
+    int getMit(String json);
+    int getSucc(String json);
+
+    void startOperation(unsigned long timeout);
+    bool isOperationTimedOut() const;
+    unsigned long operationDuration() const;
 };
 
 #endif
