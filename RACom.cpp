e79efb72365b91add2d84345519ca470232468b0
EmanueleSc <scalaemanuele92@gmail.com>
2019-03-15 16:10:28 +0100

    fix

--- RACom.cpp
+++ RACom.cpp
@@ -86,7 +86,8 @@ void RACom::broadcastPhase() {
     }
 
     if(strlen(_buffer) != 0 && getSucc() == MY_ID) {
-      Serial.println("IT'S MEEEEEEEEEEEE");
+      Serial.print("IT'S MEEEEEEEEEEEE: ");
+      Serial.println(getSucc());
       currSucc = MY_ID;
       isMyTurn = true;
     } 
